192104ca7f3fcc4ba909bab69c5dff2378b40bcb
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2025-11-02 10:27:02 +0100

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,13 +24,13 @@ body {
 .app-container {
   display: flex;
   height: 100vh;
-  background: #f0f2f5; /* gris pastel doux */
+  background: #f0f2f5; 
   font-family: 'Poppins', sans-serif;
 }
 
 .sidebar {
   width: 200px;
-  background: #e2e6eb; /* gris légèrement plus foncé que le fond */
+  background: #e2e6eb; 
   border-right: 1px solid #cbd5e1;
   padding: 1rem;
   box-shadow: 2px 0 6px rgba(0, 0, 0, 0.05);
